eb4a74f62553d9706154c0d1628634f60413456f
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2011-10-26 14:31:58 -0700

    regsim: move channel definition to regulatory.h
    This lets us make regulatory.h independent of the wireless core.
    The idea behind this is to enable a set of regulatory library helpers
    to be to used independently of the wireless core. We will then have
    a wireless core with a regulatory core that uses a regulatory library:
    
    |-----------------------------------
    | wireless core |  regulatory core |
    ------------------------------------
                             |
                             |
                             |
                             v
                   ----------------------
                   | regulatory library |
                   ----------------------
    
    With this split up we can define helper data structures within
    the regulatory library that the wireless core and regulatory can
    use independently of the functional aspects of the regulatory
    library.
    
    The regulatory library can then be used in userspace or kernelspace
    with its own set of control framework for event management / locking
    without changing. The library also enables uses of the same code in
    any OS with little or no porting.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- regulatory.h
+++ regulatory.h
@@ -8,13 +8,11 @@
 #include "ieee80211.h"
 #include "list.h"
 #include "c-hacks.h"
-#include "wifi-dev.h"
 
 /*
  * regulatory support structures
  *
- * This file contains data which is independent of any infrastructure
- * definitions of a channel or a wireless device.
+ * This file contains data which is used internally by the regulatory core.
  *
  * Copyright 2008-2009	Luis R. Rodriguez <mcgrof@qca.qualcomm.com>
  *
@@ -31,6 +29,79 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+/**
+ * enum ieee80211_channel_flags - channel flags
+ *
+ * Channel flags set by the regulatory control code.
+ *
+ * @IEEE80211_CHAN_DISABLED: This channel is disabled.
+ * @IEEE80211_CHAN_PASSIVE_SCAN: Only passive scanning is permitted
+ *	on this channel.
+ * @IEEE80211_CHAN_NO_IBSS: IBSS is not allowed on this channel.
+ * @IEEE80211_CHAN_RADAR: Radar detection is required on this channel.
+ * @IEEE80211_CHAN_NO_HT40PLUS: extension channel above this channel
+ *	is not permitted.
+ * @IEEE80211_CHAN_NO_HT40MINUS: extension channel below this channel
+ *	is not permitted.
+ */
+enum ieee80211_channel_flags {
+	IEEE80211_CHAN_DISABLED         = 1<<0,
+	IEEE80211_CHAN_PASSIVE_SCAN     = 1<<1,
+	IEEE80211_CHAN_NO_IBSS          = 1<<2,
+	IEEE80211_CHAN_RADAR            = 1<<3,
+	IEEE80211_CHAN_NO_HT40PLUS      = 1<<4,
+	IEEE80211_CHAN_NO_HT40MINUS     = 1<<5,
+};
+
+#define IEEE80211_CHAN_NO_HT40 \
+	(IEEE80211_CHAN_NO_HT40PLUS | IEEE80211_CHAN_NO_HT40MINUS)
+
+/**
+ * struct ieee80211_channel - channel definition
+ *
+ * This structure describes a single channel within a band.
+ *
+ * @center_freq: center frequency in MHz
+ * @hw_value: hardware-specific value for the channel
+ * @flags: channel flags from &enum ieee80211_channel_flags.
+ * @orig_flags: channel flags at registration time, used by regulatory
+ *	code to support devices with additional restrictions
+ * @band: band this channel belongs to.
+ * @max_antenna_gain: maximum antenna gain in dBi
+ * @max_power: maximum transmission power (in dBm)
+ * @beacon_found: helper to regulatory code to indicate when a beacon
+ *	has been found on this channel. This is useful only on 5 GHz band.
+ * @orig_mag: internal use
+ * @orig_mpwr: internal use
+ */
+struct ieee80211_channel {
+	enum ieee80211_band band;
+	uint16_t center_freq;
+	uint16_t hw_value;
+	uint32_t flags;
+	int max_antenna_gain;
+	int max_power;
+	bool beacon_found;
+	uint32_t orig_flags;
+	int orig_mag, orig_mpwr;
+};
+
+/**
+ * struct ieee80211_supported_band - frequency band definition
+ *
+ * This structure describes a frequency band a wiphy
+ * is able to operate in.
+ *
+ * @channels: Array of channels the hardware can operate in
+ *	in this band.
+ * @band: the band this structure represents
+ * @n_channels: Number of channels in @channels
+ */
+struct ieee80211_supported_band {
+        struct ieee80211_channel *channels;
+        enum ieee80211_band band;
+        int n_channels;
+};
 
 /**
  * enum environment_cap - Environment parsed from country IE
@@ -128,6 +199,5 @@ struct ieee80211_regdomain {
 
 int regulatory_init(void);
 int ieee80211_frequency_to_channel(int freq);
-void regulatory_update(struct wifi_dev *dev, enum ieee80211_reg_initiator);
 
 #endif
--- wifi-dev.h
+++ wifi-dev.h
@@ -4,86 +4,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "ieee80211.h"
+#include "regulatory.h"
 
 /*
- * Device driver channel definition templates for an 802.11 subsystem.
+ * Device template for an 802.11 subsystem.
  */
-
-/**
- * enum ieee80211_channel_flags - channel flags
- *
- * Channel flags set by the regulatory control code.
- *
- * @IEEE80211_CHAN_DISABLED: This channel is disabled.
- * @IEEE80211_CHAN_PASSIVE_SCAN: Only passive scanning is permitted
- *	on this channel.
- * @IEEE80211_CHAN_NO_IBSS: IBSS is not allowed on this channel.
- * @IEEE80211_CHAN_RADAR: Radar detection is required on this channel.
- * @IEEE80211_CHAN_NO_HT40PLUS: extension channel above this channel
- *	is not permitted.
- * @IEEE80211_CHAN_NO_HT40MINUS: extension channel below this channel
- *	is not permitted.
- */
-enum ieee80211_channel_flags {
-	IEEE80211_CHAN_DISABLED         = 1<<0,
-	IEEE80211_CHAN_PASSIVE_SCAN     = 1<<1,
-	IEEE80211_CHAN_NO_IBSS          = 1<<2,
-	IEEE80211_CHAN_RADAR            = 1<<3,
-	IEEE80211_CHAN_NO_HT40PLUS      = 1<<4,
-	IEEE80211_CHAN_NO_HT40MINUS     = 1<<5,
-};
-
-#define IEEE80211_CHAN_NO_HT40 \
-	(IEEE80211_CHAN_NO_HT40PLUS | IEEE80211_CHAN_NO_HT40MINUS)
-
-/**
- * struct ieee80211_channel - channel definition
- *
- * This structure describes a single channel for use
- * with cfg80211.
- *
- * @center_freq: center frequency in MHz
- * @hw_value: hardware-specific value for the channel
- * @flags: channel flags from &enum ieee80211_channel_flags.
- * @orig_flags: channel flags at registration time, used by regulatory
- *	code to support devices with additional restrictions
- * @band: band this channel belongs to.
- * @max_antenna_gain: maximum antenna gain in dBi
- * @max_power: maximum transmission power (in dBm)
- * @beacon_found: helper to regulatory code to indicate when a beacon
- *	has been found on this channel. This is useful only on 5 GHz band.
- * @orig_mag: internal use
- * @orig_mpwr: internal use
- */
-struct ieee80211_channel {
-	enum ieee80211_band band;
-	uint16_t center_freq;
-	uint16_t hw_value;
-	uint32_t flags;
-	int max_antenna_gain;
-	int max_power;
-	bool beacon_found;
-	uint32_t orig_flags;
-	int orig_mag, orig_mpwr;
-};
-
-/**
- * struct ieee80211_supported_band - frequency band definition
- *
- * This structure describes a frequency band a wiphy
- * is able to operate in.
- *
- * @channels: Array of channels the hardware can operate in
- *	in this band.
- * @band: the band this structure represents
- * @n_channels: Number of channels in @channels
- */
-struct ieee80211_supported_band {
-        struct ieee80211_channel *channels;
-        enum ieee80211_band band;
-        int n_channels;
-};
 
 struct device;
 
@@ -103,4 +28,7 @@ struct device {
 	struct dev_ops *ops;
 	struct wifi_dev *wdev;
 };
+
+void regulatory_update(struct wifi_dev *dev, enum ieee80211_reg_initiator);
+
 #endif /* __WIFI_DEV_H */
